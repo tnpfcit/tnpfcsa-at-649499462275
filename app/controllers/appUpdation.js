826132e410c79af721aab58f712b35122e51b931
ramesha777 <ramesha.c@fincuro.in>
2022-04-03 04:32:21 +0530

    Merged changes between prod and test servers

--- app/controllers/appUpdation.js
+++ app/controllers/appUpdation.js
@@ -1,9 +1,20 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
+var logger = require('../config/logger');
 var appUpdation = require('../config/env.json');
-var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode} = require('../config/env');
+var {sucessCode} = require('../config/env');
 
 
 exports.appupdate = (req,res) =>{
-    return res.status(200).send({"responseCode":sucessCode,"response":appUpdation});
+    logger.info(`
+        ${new Date()} || 
+        ${req.originalUrl} || 
+        ${JSON.stringify(req.body)} || 
+        ${req.ip} || 
+        ${req.protocol}
+    `);
+    return res.status(200).send({
+        "responseCode":sucessCode,
+        "response":appUpdation
+    });
 }
