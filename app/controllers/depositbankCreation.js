188b1821105615025f9c8ae2bd314fd73ffaf71a
ramesha777 <ramesha.c@fincuro.in>
2022-04-03 06:24:58 +0530

    Merged test and prod server code

--- app/controllers/depositbankCreation.js
+++ app/controllers/depositbankCreation.js
@@ -1,45 +1,65 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
-//const deptvalidation = require('../middleware/depositbankvalidation.js');
+var logger = require('../config/logger');
 var deposit_bank_creation = db.depositbankcreation;
-//const { validationResult } = require('express-validator');
+var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode} = require('../config/env');
 
 
 exports.depositbank = (req,res) =>{
-
-   // const errors = validationResult(req);
-    //deptvalidation.validates('deposit');
-    //if (!errors.isEmpty()) {res.status(200).json({"message":"ok", "responseCode":"405",errors: errors.array()});return;}
-
-    var {depositNumber,bankAccountNumber,bankCode,bankName,branchName,accHolderName,ifscCode,micrCode} = req.body;
-    //console.log(depositNumber);
-
-    deposit_bank_creation.findAll({where:{ACCOUNT_NO: bankAccountNumber}}).then(results=>{
-        if(results.length>0)
-        {
-            return res.status(200).send({"message":"ok","responseCode":"200","response":"accountNumber already exists"});
-        }
-        else
-        {
-                deposit_bank_creation.build({ 
-                DEPOSIT_NO: depositNumber, 
-                ACCOUNT_NO:bankAccountNumber,
-                BANK_CODE:bankCode,
-                BANK_NAME:bankName,
-                BRANCH_NAME:branchName,
-                CUST_NAME:accHolderName,
-                IFSC_CODE:ifscCode,
-                MICR_CODE:micrCode,
-                NEFT_ECS:'N',
-                STATUS:"CREATED",
-                STATUS_BY:"SYSUSER",
-                STATUS_DT:new Date(),
-                 }).save().then(results => {
-                  //console.log(results);
-                  return res.status(200).send({"message":"ok","responseCode":"200","response":"data saved sucessfully"})
-                 }).catch(err => {res.status(200).send({message: err.message});});
-
-        }
-
-    }).catch(err => {res.status(200).send({message: err.message});});
-}
+		let {
+			depositNumber,
+			bankAccountNumber,
+			bankCode,
+			bankName,
+			branchName,
+			accHolderName,
+			ifscCode,
+			micrCode
+		} = req.body;
+		logger.info(`
+			${new Date()} || 
+			${req.originalUrl} || 
+			${JSON.stringify(req.body)} || 
+			${req.ip} || 
+			${req.protocol} || 
+			${req.method}
+		`);
+		deposit_bank_creation.findAll({where:{ACCOUNT_NO: bankAccountNumber}}).then(results=>{
+			if(results.length>0){
+				return res.status(200).send({
+                "responseCode":sucessCode,
+                "response":"AccountNumber already exists"
+				});
+			}
+			else{
+				deposit_bank_creation.build({ 
+				DEPOSIT_NO: depositNumber, 
+				ACCOUNT_NO:bankAccountNumber,
+				BANK_CODE:bankCode,
+				BANK_NAME:bankName,
+				BRANCH_NAME:branchName,
+				CUST_NAME:accHolderName,
+				IFSC_CODE:ifscCode,
+				MICR_CODE:micrCode,
+				NEFT_ECS:'N',
+				STATUS:"CREATED",
+				STATUS_BY:"SYSUSER",
+				STATUS_DT:new Date(),
+				}).save().then(results => {
+					return res.status(200).send({
+                        "responseCode":sucessCode,
+                        "response":"data saved sucessfully"
+                    });
+				}).catch(err => {
+					return res.status(500).send({
+					data:null, message: err.message
+					});
+				});
+			}
+		}).catch(err => { 
+			return res.status(500).send({
+            data:null, 
+            message: err.message
+			});
+		});
+	}
