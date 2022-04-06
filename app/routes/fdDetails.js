f89db9edfa171d20ccf36df8bcc2c8eaef5f7a83
ramesha777 <ramesha.c@fincuro.in>
2022-04-06 23:08:59 +0530

    Merged live and test server code

--- app/routes/fdDetails.js
+++ app/routes/fdDetails.js
@@ -1,5 +1,4 @@
 module.exports = function(app) {
-	   
     const fddetails = require('../controllers/fdDetails.js');
-    app.post('/tnpfc/v1/getallFdDetails',app.oauth.authenticate(), fddetails.findAll);
+    app.post('/tnpfc/v1/getallFdDetails',app.oauth.authenticate(), fddetails.depositDetails);
 }
