47b4bfff7c9de20e2ed91bfb7aa2899835c5a05c
ramesha777 <ramesha.c@fincuro.in>
2022-04-06 23:40:39 +0530

    Merged live and test server code

--- app/controllers/financialYear.js
+++ app/controllers/financialYear.js
@@ -4,9 +4,6 @@ var logger = require('../config/logger');
 var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode,NoRecords} = require('../config/env');
 
 exports.financialyear = (req, res, err) => {
-    
-    var customerId = 'C000787878';
-
     logger.info(`
       ${new Date()} || 
       ${req.originalUrl} || 
@@ -15,42 +12,33 @@ exports.financialyear = (req, res, err) => {
       ${req.protocol} || 
       ${req.method}
     `);
-
-    //var result = [{fyCode:"20-21", fyYear:"2020-2021"},{fyCode:"21-22",fyYear:"2021-2022"}];
-
-    //return res.status(200).send({"responseCode":sucessCode,"response":result});
-
-    if(customerId) {
-      
-      var query = 'SELECT API_GETFINYEAR(-1) as "fin" FROM DUAL\ UNION\ SELECT API_GETFINYEAR(0) "fin" FROM DUAL'; 
-      
-      // query to fetch results from db
-      db.sequelize.query(query,{type: sequelize.QueryTypes.SELECT}
-      ).then(results => {
-        console.log("================results"+JSON.stringify(results));
-
-        if(results.length > 0){
-          //var result = [{fyCode:"20-21", fyYear:results[0].fin},{ fyCode:"21-22",fyYear:results[1].fin}];
-          return res.status(200).send({
+	var query = 'SELECT API_GETFINYEAR(-4) "fin" FROM DUAL\
+		UNION\
+		SELECT API_GETFINYEAR(-3) "fin" FROM DUAL\
+		UNION \
+		SELECT API_GETFINYEAR(-2) "fin" FROM DUAL\
+		UNION\
+		SELECT API_GETFINYEAR(-1) as "fin" FROM DUAL \
+		UNION \
+		SELECT API_GETFINYEAR(0) "fin" FROM DUAL'; 
+	db.sequelize.query(query,{type: sequelize.QueryTypes.SELECT}
+	).then(results => {
+		if(results.length > 0){
+			return res.status(200).send({
             "responseCode":sucessCode,
-            "response":[{fyCode:"20-21", fyYear:results[0].fin},{ fyCode:"21-22",fyYear:results[1].fin}]
-          });
-        } else {
-          return res.status(200).send({
-            "responseCode":resourceNotFoundcode,
-            "response":NoRecords
-          }); 
-        }
-      }).catch(err => {
-        res.status(500).send({
-          data:null,
-          message: err.message
-        });
-      });
-    } else {
-      return res.status(200).send({
-        "responseCode":badRequestcode,
-        "response":responseMessage
-      }); 
-    }
-}    
+            "response":[{fyCode:results[0].fin, fyYear:results[0].fin},{ fyCode:results[1].fin,fyYear:results[1].fin},
+			{ fyCode:results[2].fin,fyYear:results[2].fin},{ fyCode:results[3].fin,fyYear:results[3].fin},{ fyCode:results[4].fin,fyYear:results[4].fin}]
+			});
+		} else {
+			return res.status(200).send({
+			"responseCode":resourceNotFoundcode,
+			"response":NoRecords
+			}); 
+		}
+	}).catch(err => {
+		return res.status(500).send({
+		data:null,
+		message: err.message
+		});
+	});
+}
