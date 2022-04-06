536a68b8f82667a8d34cf615722e9c46c80fcad3
ramesha777 <ramesha.c@fincuro.in>
2022-04-06 23:04:43 +0530

    Merged live and test server code

--- app/controllers/fdDetails.js
+++ app/controllers/fdDetails.js
@@ -1,28 +1,48 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
+var logger = require('../config/logger');
+var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode,NoRecords} = require('../config/env');
 
-exports.findAll = (req, res) => {
-var customerId = req.body.customerId;
-if(customerId){
-db.sequelize.query('select "accountNumber", "productDesc", "productId", "openDate", "maturityDate", "interestAmount", "annualInterest",  "interestRatePercent", "depositAmount", "maturityAmount", "productAliasName","accountStatus", "eFdrUrl", "depositAccountType" from table (get_cust_fd_details(:customerId)) where "customerId"=:customerId',
-    { replacements: { customerId: customerId }, type: sequelize.QueryTypes.SELECT }
-  ).then(results => {
-    
-    if(results.length>0)
-    {
-        return res.status(200).send({"message": "ok","responseCode":"200","response":results});
-    }
-    else{
-      res.send({"responseCode":"404","message": "customer details not found "}); 
-    }
-   
-  }).catch(err => {
-    res.status(500).send({
-        message: err.message
-    });
-  });
+exports.depositDetails = (req, res, err) => {
+	let customerId = req.body.customerId;
+	logger.info(`
+		${new Date()} || 
+		${req.originalUrl} || 
+		${JSON.stringify(req.body)} || 
+		${req.ip} || 
+		${req.protocol} || 
+		${req.method}
+	`);
+	if(customerId){
+		var query = 'select "accountNumber", "productDesc", "productId", "openDate",\
+		"maturityDate", "interestAmount", "annualInterest", "interestRatePercent",\
+		"depositAmount", "maturityAmount", "productAliasName", "accountStatus",\
+		"eFdrUrl", "depositAccountType" from table (get_cust_fd_details(:customerId)) where "customerId"=:customerId'; 
+		db.sequelize.query(query,{replacements: {customerId:customerId },type: sequelize.QueryTypes.SELECT}
+		).then(results => {
+			if(results.length>0){
+				return res.status(200).send({
+				"responseCode":sucessCode,
+				"response":results
+				});
+			}
+			else{
+				return res.status(200).send({
+				"responseCode":resourceNotFoundcode,
+				"response":NoRecords
+				}); 
+			}
+		}).catch(err => {
+			logger.error("fd query results error ======"+ err);
+			res.status(500).send({
+			data:null,
+			message: err.message
+			});
+		});
+	}else{
+		return res.status(200).send({
+        "responseCode":badRequestcode,
+        "response":responseMessage
+      }); 
+	}
 }
-else{
-  res.send({"responseCode":"401","message": "Invalid input parameters. Please check the key value pair in the request body."}); 
-}
-  };
