51c2ad25996ee344695244d869eb02335b2cbe5f
karthi.s <karthi.s@fincuro.com>
2023-05-13 18:17:20 +0530

    commented sms while logging out

--- app/controllers/logout.js
+++ app/controllers/logout.js
@@ -22,13 +22,13 @@ exports.logout = (req,res) => {
                     var phNumber = results[0].PHONE_NUMBER;
                     var currDate = String(moment(new Date()).format('DD-MM-YYYY HH:mm:ss'));
                     logut.update({LOG_OUT:currDate},{ where: {USER_ID:customerId}}).then(results => {
-						if(channel == 'app'){
+						/*if(channel == 'app'){
                             var msg = urlencode('Dear '+depName+', You have logged-out of TNPFCL Mobile App on '+currDate+' For any issues contact our 24x7 interactive voice response system phone number 044-46312345');
                         } else {
                             var msg = urlencode('Dear '+depName+', You have logged-out of TNPF Web Portal for Depositors on '+currDate+' For any issues contact our 24x7 interactive voice response system phone number 044-46312345');
                         }
                         var data = 'username=' + username + '&hash=' + hash + '&sender=' + sender + '&numbers=' + phNumber + '&message=' + msg;
-                        request.get("https://api.textlocal.in/send?"+ data, (error, response, body) => {});
+                        request.get("https://api.textlocal.in/send?"+ data, (error, response, body) => {}); - done on dec 16 tnpfc 2211*/
                         return res.status(200).send({
                             "responseCode":sucessCode,
                             "response":"You are Sucessfully Logged Out"
