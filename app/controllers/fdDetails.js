f85a73a6044abc8e6081acd69d532ad60e59fb09
karthi.s <karthi.s@fincuro.com>
2023-05-13 17:28:01 +0530

    merged live code

--- app/controllers/fdDetails.js
+++ app/controllers/fdDetails.js
@@ -5,44 +5,46 @@ var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode,NoRecords} =
 
 exports.depositDetails = (req, res, err) => {
 	let customerId = req.body.customerId;
-	logger.info(`
-		${new Date()} || 
-		${req.originalUrl} || 
-		${JSON.stringify(req.body)} || 
-		${req.ip} || 
-		${req.protocol} || 
-		${req.method}
-	`);
+	  logger.info(`
+      ${new Date()} || 
+      ${req.originalUrl} || 
+      ${JSON.stringify(req.body)} || 
+      ${req.ip} || 
+      ${req.protocol} || 
+      ${req.method}
+    `);
 	if(customerId){
-		var query = 'select "accountNumber", "productDesc", "productId", "openDate",\
-		"maturityDate", "interestAmount", "annualInterest", "interestRatePercent",\
-		"depositAmount", "maturityAmount", "productAliasName", "accountStatus",\
-		"eFdrUrl", "depositAccountType" from table (get_cust_fd_details(:customerId)) where "customerId"=:customerId'; 
-		db.sequelize.query(query,{replacements: {customerId:customerId },type: sequelize.QueryTypes.SELECT}
-		).then(results => {
-			if(results.length>0){
-				return res.status(200).send({
+		let query='select "accountNumber", "productDesc", "productId", "openDate",\
+		"maturityDate", "interestAmount", "annualInterest",  "interestRatePercent",\
+		"depositAmount", b.actual_maturity_amt "maturityAmount", "productAliasName",\
+		"accountStatus","eFdrUrl", "depositAccountType",deposit_period_mm as "tenure"\
+		 from table (get_cust_fd_details(:customerId)) a join deposit_sub_acinfo b on \
+		 a."accountNumber" = b.deposit_no where "customerId"=:customerId';
+	db.sequelize.query(query,
+		{replacements: {customerId:customerId },type: sequelize.QueryTypes.SELECT}
+	  ).then(results => {
+		if(results.length>0){
+			return res.status(200).send({
 				"responseCode":sucessCode,
 				"response":results
 				});
-			}
-			else{
-				return res.status(200).send({
-				"responseCode":resourceNotFoundcode,
-				"response":NoRecords
-				}); 
-			}
-		}).catch(err => {
-			logger.error("fd query results error ======"+ err);
-			res.status(500).send({
+		}else{
+			return res.status(200).send({
+			  "responseCode":resourceNotFoundcode,
+			  "response":NoRecords
+			  }); 
+		}
+	  }).catch(err => {
+			return res.status(500).send({
 			data:null,
-			message: err.message
+			"message": err.message
 			});
-		});
-	}else{
-		return res.status(200).send({
-        "responseCode":badRequestcode,
-        "response":responseMessage
-      }); 
+	  });
 	}
-}
+	else{
+	  return res.status(200).send({
+		  "responseCode":badRequestcode,
+		  "message": responseMessage
+		  }); 
+	}
+ }
