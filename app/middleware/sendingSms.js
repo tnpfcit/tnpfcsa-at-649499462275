5838891b018bdb587701162681d21bbed9154189
karthi.s <karthi.s@fincuro.com>
2023-05-13 18:54:17 +0530

    changed sms server code

--- app/middleware/sendingSms.js
+++ app/middleware/sendingSms.js
@@ -4,23 +4,23 @@ var request = require('request');
 const logger = require('../config/logger');
 
 function loginMessage (phNumber,otpNumber,depName,channelName) {
-
-    var apikey = 'fKg1OFur8W0-o5BZOYix5QH819nWmckJyUIfCoqobz';
-    
-    if (channelName =='app'){
-        var msg = urlencode('Dear '+depName+', '+otpNumber+' is your OTP for TNPFCL Mobile App login and valid till 5 minutes. Do not share this OTP to anyone for security reasons.');	
+	
+	console.log( "inside sms code");
+	if (channelName =='app'){
+        var msg = urlencode('Dear '+depName+', '+otpNumber+' is your OTP for TNPFIDCL Mobile app login and valid for 5 minutes. Keep the OTP confidential-TNPFIDC');	
     } else {
-        var msg = urlencode('Dear '+depName+', '+otpNumber+' is your OTP for TNPFCL Webportal login and valid till 5 minutes. Do not share this OTP to anyone for security reasons.');
-    }   
-    
-    
-    var sender='TNPFCL';
-    var data='apikey='+apikey+'&sender='+sender+'&numbers='+phNumber+'&message='+msg
+        var msg = urlencode('Dear '+depName+', OTP for login is '+otpNumber+'. Its validity expires in 5 minutes and keep it confidential.-TNPFIDC');
+    }
+    /*if (channelName =='app'){
+        var msg = urlencode('Dear '+depName+', '+otpNumber+' is your OTP for TNPFCL Mobile App login and valid till 5 minutes. Do not share this OTP to anyone for security reasons.-Tamil Nadu Power Finance (TNPF)');	
+    } else {
+        var msg = urlencode('Dear '+depName+', '+otpNumber+' is your OTP for TNPFCL Webportal login and valid till 5 minutes. Do not share this OTP to anyone for security reasons.-Tamil Nadu Power Finance (TNPF)');
+    }*/
+	var data = 'APIKey=6IBUmYiLRk659H5Blt03RQ&senderid=TNPFFD&channel=Trans&DCS=0&flashsms=0&number='+phNumber+'&text='+msg+'&route=6';
     var options = {
-                    host: 'api.textlocal.in',
-                    path: '/send?'+data
+		host: '182.18.143.11',
+		path: '/api/mt/SendSMS?'+data
     };
-
     callback = function(response) {
         var str = '';
         response.on('data', function (chunk) {
@@ -33,6 +33,6 @@ function loginMessage (phNumber,otpNumber,depName,channelName) {
     }
      
     http.request(options, callback).end();
-}                
+    }                
 
 module.exports = loginMessage;
