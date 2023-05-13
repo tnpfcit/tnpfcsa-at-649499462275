b2335793ec7e7d264a20aeac1e2c24093424ec1e
karthi.s <karthi.s@fincuro.com>
2023-05-13 19:09:56 +0530

    merged live code

--- app/routes/otpLogin.js
+++ app/routes/otpLogin.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
     const otplogin = require('../controllers/otpLogin.js');
-    app.post('/tnpfc/v1/otplogin', otplogin.logincreation)
+    app.post('/tnpfc/v1/otplogin', otplogin.logincreation);
+	app.post('/tnpfc/v1/sendFmsSMS', otplogin.sendsms);
    
 }
