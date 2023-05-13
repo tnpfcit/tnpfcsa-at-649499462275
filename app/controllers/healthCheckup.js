dc4feb8e8546f3e514c46072b3d14c9b2a42d443
karthi.s <karthi.s@fincuro.com>
2023-05-13 22:29:36 +0530

    merged test code to live

--- app/controllers/healthCheckup.js
+++ app/controllers/healthCheckup.js
@@ -1,17 +1,18 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
-exports.check = (req, res) => {
- db.sequelize.query('select 1 from dual').then(results => {
- 
-    return res.status(200).send({"message": "ok","responseCode":"200"});
- 
- }).catch(err => {
-    res.status(500).send({
-        message: err.message
-    });
-  });
- };
-  
- 
-  
-  
+var logger = require('../config/logger');
+var {sucessCode} = require('../config/env');
+
+exports.check = (req,res,err) => {
+   db.sequelize.query('select 1 from dual').then(results => {
+      return res.status(200).send({
+         "message":"ok",
+         "responseCode":sucessCode
+      });
+   }).catch(err => {
+      return res.status(500).send({
+         data:null,
+         message: err.message
+      });
+   });
+}
