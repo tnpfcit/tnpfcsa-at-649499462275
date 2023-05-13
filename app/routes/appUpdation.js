f4d0be2034e9c7fe157e6b6d160fd17f355bb0d3
karthi.s <karthi.s@fincuro.com>
2023-05-13 19:01:02 +0530

    merged live code

--- app/routes/appUpdation.js
+++ app/routes/appUpdation.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
 	   
     const updation = require('../controllers/appUpdation.js');
-    app.get('/tnpfc/v1/check-mobile-app-version', updation.appupdate);
+    app.post('/tnpfc/v1/check-mobile-app-version', updation.appupdate);
 }
