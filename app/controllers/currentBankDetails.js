57022a4326ae2a5e19e3cdbcf47ad7f8024a1b2d
karthi.s <karthi.s@fincuro.com>
2023-05-13 21:55:49 +0530

    merged test with live

--- app/controllers/currentBankDetails.js
+++ app/controllers/currentBankDetails.js
@@ -5,46 +5,48 @@ var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode,NoRecords} =
 
 
 exports.existingBankDetails = (req,res) =>{
-		var customerId = req.body.customerId;
-		logger.info(`
-			${new Date()} || 
-			${req.originalUrl} || 
-			${JSON.stringify(req.body)} || 
-			${req.ip} || 
-			${req.protocol}
-		`);
-		
-		if(!customerId){
+    var customerId = req.body.customerId;
+    logger.info(`
+        ${new Date()} || 
+        ${req.originalUrl} || 
+        ${JSON.stringify(req.body)} || 
+        ${req.ip} || 
+        ${req.protocol}
+    `);
 
-			return res.status(200).send({
-				"responseCode":badRequestcode,
-				"response":responseMessage
-			});
-		}
-		 
-		var query = 'select upper(cust_name) "accountHolderName", upper(bank_name) "bankName",\
-		NVL (upper(branch_name), NULL) "branchName", upper(ifsc_code) "ifscCode", account_no "bankAccountNumber", GETBANKBRANCHADDRESS(upper(ifsc_code)) "bankBranchAddress" from deposit_other_bank_details\
-		where deposit_no in (select deposit_no from deposit_acinfo where cust_id =:customerId) group by cust_name,bank_name,branch_name,upper(ifsc_code),account_no';
+    if(!customerId){
 
-		db.sequelize.query(query,{replacements:{customerId:customerId},type: sequelize.QueryTypes.SELECT}
-		).then(results=>{
-			if(results.length > 0){
-				return res.status(200).send({
-				"responseCode":sucessCode,
-				"response":results
-				});
-			} else {
-				return res.status(200).send({
-				"responseCode":resourceNotFoundcode,
-				"response":[],
-				"message":NoRecords
-				});
-			}
-			}).catch(err => {
-				logger.error(err);
-				return res.status(500).send({
-				data:null,
-				message: err.message
-				});
-			});
-	}
+        return res.status(200).send({
+            "responseCode":badRequestcode,
+            "response":responseMessage
+        });
+    }
+     
+    var query = 'select TRIM(upper(ifsc_code)) "ifscCode", TRIM(upper(cust_name)) "accountHolderName", TRIM(upper(bank_name)) "bankName",\
+    NVL (TRIM(upper(branch_name)), NULL) "branchName", \
+    TRIM(account_no) "bankAccountNumber", GETBANKBRANCHADDRESS(TRIM(upper(ifsc_code))) "bankBranchAddress" from deposit_other_bank_details\
+    where deposit_no in (select deposit_no from deposit_acinfo where cust_id =:customerId) \
+    group by TRIM(upper(cust_name)),TRIM(upper(bank_name)),NVL(TRIM(UPPER(branch_name)),NULL),TRIM(upper(ifsc_code)),TRIM(account_no)';
+
+    db.sequelize.query(query,{replacements:{customerId:customerId},type: sequelize.QueryTypes.SELECT}
+    ).then(results=>{            
+            if(results.length > 0){
+                return res.status(200).send({
+                    "responseCode":sucessCode,
+                    "response":results
+                });
+            } else {
+                return res.status(200).send({
+                    "responseCode":resourceNotFoundcode,
+                    "response":[],
+                    "message":NoRecords
+                });
+            }
+    }).catch(err => {
+            logger.error(err);
+            return res.status(500).send({
+                data:null,
+                message: err.message
+            });
+    });   
+}
