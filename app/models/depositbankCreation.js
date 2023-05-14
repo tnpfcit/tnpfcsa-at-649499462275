0ffd9b61f35830dd2e8d386999312117beb1fb46
karthi.s <karthi.s@fincuro.com>
2023-05-14 11:15:12 +0530

    merged test code to live

--- app/models/depositbankCreation.js
+++ app/models/depositbankCreation.js
@@ -3,19 +3,11 @@ const validator = require('validator');
 module.exports = (sequelize, DataTypes) => {
 const depositbankCreation= sequelize.define('depositbankcreation', {
 
-     DEPOSIT_NO:{
+    DEPOSIT_NO:{
          type:DataTypes.STRING,
-         //allowNull:false,
-        //  validate: {
-        //     len: [13, 13],
-        //     isInt:true
-        //  }
-     },
+    },
     ACCOUNT_NO:{
         type: DataTypes.STRING,
-        // validate: {
-        //     isInt:true
-        //  }
     },
     BANK_CODE:DataTypes.DECIMAL(10),
     BANK_NAME:DataTypes.STRING,
@@ -27,16 +19,11 @@ const depositbankCreation= sequelize.define('depositbankcreation', {
     STATUS:DataTypes.STRING,
     STATUS_BY:DataTypes.STRING,
     STATUS_DT:DataTypes.DATE,
-    // RELATIONSHIP:DataTypes.STRING,
-    // STREET:DataTypes.STRING
-},
+    },
     {
        underscored: true,
        tableName:'DEPOSIT_OTHER_BANK_DETAILS'
-      
-     
     });
-        //depositCreation.schema("TNPFC");
-        depositbankCreation.removeAttribute("id");
-        return depositbankCreation;
-   };
+    depositbankCreation.removeAttribute("id");
+    return depositbankCreation;
+}
