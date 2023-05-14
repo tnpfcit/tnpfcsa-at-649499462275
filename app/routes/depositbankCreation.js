3695421c2824ec0c0e244c111025b1c4f5d1a074
karthi.s <karthi.s@fincuro.com>
2023-05-14 11:24:07 +0530

    merged test code to live

--- app/routes/depositbankCreation.js
+++ app/routes/depositbankCreation.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
 	   
     const depositbankcreation = require('../controllers/depositbankCreation.js');
-    //const depositvalidationn = require('../middleware/depositbankvalidation.js');
     app.post('/tnpfc/v1/updateBankDetails', depositbankcreation.depositbank);
 }
