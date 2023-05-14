eca247dabe551af7aaf25850ba2a7dfebe15f473
karthi.s <karthi.s@fincuro.com>
2023-05-14 08:05:56 +0530

    merged test code to live

--- app/controllers/payStatisticsSummary.js
+++ app/controllers/payStatisticsSummary.js
@@ -1,28 +1,39 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
 const logger = require('../config/logger');
-var {responseMessage,sucessCode,resourceNotFoundcode,badRequestcode,NoRecords} = require('../config/env');
+var {
+    responseMessage,
+    sucessCode,
+    resourceNotFoundcode,
+    badRequestcode,
+    NoRecords
+} = require('../config/env');
 
 exports.statisticsSummary = (req,res,err) => {
 
-    logger.info(`${new Date()} || ${req.originalUrl} || ${JSON.stringify(req.body)} || ${req.ip} || ${req.protocol}`);
+    logger.info(`
+        ${new Date()} || 
+        ${req.originalUrl} || 
+        ${JSON.stringify(req.body)} || 
+        ${req.ip} || 
+        ${req.protocol}
+    `);
     var query = 'select bank_name "bankName",successful_trans_count "sucessCount",successful_trans_percent "sucessPercent",\
                 failure_trans_count "failureCount",failure_trans_percent "failurePercent"\
                 from payment_statistics_summary order by successful_trans_count desc';
     
-    db.sequelize.query(query,{type: sequelize.QueryTypes.SELECT}
+    db.sequelize.query(query,{type:sequelize.QueryTypes.SELECT}
     ).then(results=>{
-        logger.info("interest results=========="+JSON.stringify(results));
-        if(results.length > 0){
+        if(results.length > 0){            
             return res.status(200).send({
                 "responseCode":sucessCode,
                 "response":results
             });
-        } else {
-            return res.status(404).send({
+        } else {            
+            return res.status(200).send({
                 "responseCode":resourceNotFoundcode,
                 "response":NoRecords
-            });
+            });            
         }
     }).catch(err => {
         logger.error(err);
