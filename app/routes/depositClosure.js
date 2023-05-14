544fff581661c112753075f7d7bab265a38584e2
karthi.s <karthi.s@fincuro.com>
2023-05-14 11:24:42 +0530

    merged test code to live

--- app/routes/depositClosure.js
+++ app/routes/depositClosure.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 	   
     const depositClosure = require('../controllers/depositClosure.js');
-    //const depositClosureValidation = require('../middleware/depositClosureValidation.js');
-    app.post('/tnpfc/v1/depositClosure', app.oauth.authenticate(), depositClosure.depositclosure);
-    //app.post('/tnpfc/v1/depositClosure',depositClosureValidation.validates('depositClosure'),depositClosure.depositclosure);
+    app.post('/tnpfc/v1/depositClosure',app.oauth.authenticate(),depositClosure.depositclosure);
+    //app.post('/tnpfc/v1/depositClosure',depositClosure.depositclosure);
+    
 }
