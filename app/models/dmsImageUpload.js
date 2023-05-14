4e3e86768dd1f72b96e68db76f7c3f216c5269b1
karthi.s <karthi.s@fincuro.com>
2023-05-14 11:15:54 +0530

    merged test code to live

--- app/models/dmsImageUpload.js
+++ app/models/dmsImageUpload.js
@@ -1,13 +1,7 @@
 'use strict'
 const db = require('../config/db.js');
-//const db1 = require('../config/db1.js');
 module.exports = (sequelize, DataTypes) => {
 const dmsImageUpload= sequelize.define('dmsImageUpload', {
-   
-    //CUST_ID: DataTypes.STRING,
-	//PROFILE_PIC: DataTypes.STRING,
-	//ADDRESS_PROOF: DataTypes.STRING
-
 	CUST_ID: DataTypes.STRING,
 	DOC_TYPE: DataTypes.STRING,
 	FILE_NAME: DataTypes.STRING
@@ -15,13 +9,8 @@ const dmsImageUpload= sequelize.define('dmsImageUpload', {
 },
     {
       underscored: true,
-		tableName:'CUSTOMER_DOC_DETAILS', 
-      //tableName:'CUSTOMER',
-      //timestamps: true,
+		  tableName:'CUSTOMER_DOC_DETAILS', 
     });
-        //otpGeneration.schema("TNPFC");
         dmsImageUpload.removeAttribute("id");
-       // otpGeneration.removeAttribute("updated_at");
-       // otpGeneration.removeAttribute("created_at");
         return dmsImageUpload;
    };
