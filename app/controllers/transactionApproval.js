f4d743bb7f545f76105e739e657702593ee6cb10
karthi.s <karthi.s@fincuro.com>
2023-05-14 10:03:06 +0530

    merged test code to live

--- app/controllers/transactionApproval.js
+++ app/controllers/transactionApproval.js
@@ -1,13 +1,25 @@
 const db = require('../config/db.js');
 const sequelize = require('sequelize');
-var logger = require('../config/logger');
 const moment = require('moment');
-//var approveMasterTable = db.approve;
+var logger = require('../config/logger');
+var {
+    sucessCode,
+    resourceNotFoundcode,
+    badRequestcode,
+    responseMessage
+} = require('../config/env');
 
 exports.transferApproval = (req,res) =>{
-    var { userId,remarks,userStatus,batchId,purpose,transDt,roleId} = req.body;
-		transDt = moment(transDt).format('DD-MMM-YYYY');
-	logger.info(`
+    var {
+        userId,
+        remarks,
+        userStatus,
+        batchId,
+        purpose,
+        transDt,
+        roleId
+    } = req.body;
+    logger.info(`
         ${res.StatusCode} || 
         ${new Date()} || 
         ${req.originalUrl} || 
@@ -15,16 +27,33 @@ exports.transferApproval = (req,res) =>{
         ${req.ip} || 
         ${req.protocol} || 
         ${req.method}
-    `);
-		console.log("dt"+transDt);
-        if(userId && remarks && userStatus && batchId && purpose){
-            db.sequelize.query('select API_IPAD_TRANSFER_APPROVAL (:userId,:remarks,:userStatus,:batchId,:purpose,:transDt,:roleId) as batchId from dual',
-                {replacements: {userId: userId, remarks: remarks, userStatus: userStatus, batchId: batchId, purpose: purpose,transDt: transDt,roleId: roleId},
-                type: sequelize.QueryTypes.SELECT}
-            ).then(results => {
-                return res.status(200).send({"responseCode":"200", "response":"Updated sucessfully"});
-            }).catch(err => {res.status(500).send({message: err.message});});
-        } else {
-            return res.status(400).send({"responseCode":"400","response":"Bad request check payload"});
-        }
+    `);        
+    transDt = moment(transDt).format('DD-MMM-YYYY');	
+    if(userId && remarks && userStatus && batchId && purpose){        
+        db.sequelize.query('select API_IPAD_TRANSFER_APPROVAL (:userId,:remarks,:userStatus,:batchId,:purpose,:transDt,:roleId) as batchId from dual',
+            {replacements: {
+                userId: userId, 
+                remarks: remarks, 
+                userStatus: userStatus, 
+                batchId: batchId, 
+                purpose: purpose,
+                transDt: transDt,
+                roleId: roleId
+            },type: sequelize.QueryTypes.SELECT}
+        ).then(results => {
+                return res.status(200).send({
+                    "responseCode":sucessCode, 
+                    "response":"Updated sucessfully"
+                });
+            }).catch(err => {
+                res.status(500).send({
+                    message: err.message
+                });
+            });
+    } else {
+        return res.status(400).send({
+            "responseCode":badRequestcode,
+            "response":responseMessage
+        });
+    }
 }
